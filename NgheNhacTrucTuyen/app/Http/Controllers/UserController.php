d5826a978aab6a95d368b332b2585e9e65f6ec78
Irinyan <81472203+hoz17@users.noreply.github.com>
2023-10-24 17:07:59 +0700

    user controller

--- NgheNhacTrucTuyen/app/Http/Controllers/UserController.php
+++ NgheNhacTrucTuyen/app/Http/Controllers/UserController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 class UserController extends Controller
 {
-    //
+    // chưa chạy được
+    public function login(Request $request){
+        $credentials = $request->only('username', 'password');
+        $user = User::where('username', $credentials['username'])->first();
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            // Trả về mã lỗi 401 nếu sai tài khoản hoặc mật khẩu
+            return response()->json(['message' => 'Sai tài khoản hoặc mật khẩu'], 401);
+        }
+        // Đăng nhập thành công, trả về mã lỗi 200 và dữ liệu người dùng (nếu cần)
+        return response()->json(['user' => $user, 'message' => 'Đăng nhập thành công'], 200);
+    //    if (Auth::attempt($credentials)) {
+    //        // Đăng nhập thành công
+    //        $user = Auth::user();
+    //        return response()->json(['user'=>$user, 'message'=> 'Đăng nhập thành công'],200);
+    //    }else {
+    //    // Đăng nhập thất bại
+    //    return response()->json(['message' => 'Đăng nhập thất bại'], 401);
+    //}
+    }
+
+    function register(){
+
+    }
+    function logout(){
+
+    }
+    function forgot(){
+
+    }
+    function changepwd(){
+
+    }
 }
--- NgheNhacTrucTuyen/routes/api.php
+++ NgheNhacTrucTuyen/routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SongController;
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\PlaylistController;
+use App\Http\Controllers\PlaylistSongController;
+use App\Http\Controllers\RankingController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +25,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// User routes
+Route::post('/login',[UserController::class,'login']);
+Route::post('/register',[UserController::class,'register']);
+Route::post('/logout',[UserController::class,'logout']);
+Route::post('/forgot',[UserController::class,'forgot']);
+Route::post('/changepwd',[UserController::class,'changepwd']);
