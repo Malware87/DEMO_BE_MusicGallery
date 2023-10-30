30dee0fa0d76e8bdf06f460552bf1eea7bae931f
Irinyan <81472203+hoz17@users.noreply.github.com>
2023-10-30 14:01:39 +0700

    1

--- NgheNhacTrucTuyen/routes/api.php
+++ NgheNhacTrucTuyen/routes/api.php
@@ -27,8 +27,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // User routes
-Route::post('/login',[UserController::class,'login']);
-Route::post('/register',[UserController::class,'register']);
-Route::post('/logout',[UserController::class,'logout']);
-Route::post('/forgot',[UserController::class,'forgot']);
-Route::post('/changepwd',[UserController::class,'changepwd']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/forgot', [UserController::class, 'forgot']);
+Route::post('/changepwd', [UserController::class, 'changepwd']);
